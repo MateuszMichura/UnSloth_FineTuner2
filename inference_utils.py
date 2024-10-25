59a2ca830a8b2042961b5e2e4598c6835647eeee
MateuszMichura <127441161+MateuszMichura@users.noreply.github.com>
2024-10-25 14:05:30 +0200

    Update inference_utils.py

--- inference_utils.py
+++ inference_utils.py
@@ -20,6 +20,6 @@ def test_model(model, tokenizer, input_text):
         return_tensors="pt"
     ).to("cuda")
 
-    outputs = model.generate(input_ids=inputs, max_new_tokens=64, use_cache=True,
-                             temperature=1.5, min_p=0.1)
+    outputs = model.generate(input_ids=inputs, max_new_tokens=64000, use_cache=True,
+                             temperature=0, min_p=0.1)
     return tokenizer.batch_decode(outputs)[0]
